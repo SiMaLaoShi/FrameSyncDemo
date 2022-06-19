d562061739875208b48fd39406758cd29d9d4feb
simalaoshi <1742495214@qq.com>
2022-06-19 16:25:09 +0800

    1，完善回放系统

---
+++ FrameClient/Assets/Scripts/BattleScene/UI/Replay/ReplayItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BattleScene.Replay;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BattleScene.UI.Replay
+{
+    public class ReplayItem : MonoBehaviour
+    {
+        public Text ReplayDesc;
+        public Button BtnReplay;
+        private string date;
+        private void Awake()
+        {
+            BtnReplay.onClick.AddListener((() =>
+            {
+                if (string.IsNullOrEmpty(date))
+                    return;
+                ReplaySystem.Instance.StartReplayInfo(date);
+            }));
+        }
+
+        public void SetView(string replayDate)
+        {
+            date = replayDate;
+            ReplayDesc.text = replayDate;
+        }
+    }
+}
---
+++ FrameClient/Assets/Scripts/BattleScene/UI/Replay/UIReplayListWnd.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using BattleScene.Replay;
+using Lib.Runtime;
+using UnityEngine;
+
+namespace BattleScene.UI.Replay
+{
+    public class UIReplayListWnd : MonoBehaviour
+    {
+        private List<ReplayItem> ReplayItems;
+        public GameObject ReplayItemGo;
+        public RectTransform ReplayListTr;
+        private readonly List<string> ReplayDates = new List<string>();
+        private void Awake()
+        {
+            LoadReplayList();
+            foreach (var date in ReplayDates)
+            {
+                var go = Instantiate(ReplayItemGo, ReplayListTr);
+                var replayItem = go.GetComponent<ReplayItem>();
+                replayItem.SetView(date);
+            }
+        }
+
+        void LoadReplayList()
+        {
+            var dir = Directory.GetDirectories(ReplaySystem.Instance.SaveReplayDir);
+            foreach (var s in dir)
+            {
+                var directoryInfo = new DirectoryInfo(s);
+                ReplayDates.Add(directoryInfo.Name);
+            }
+        }
+    }
+}
