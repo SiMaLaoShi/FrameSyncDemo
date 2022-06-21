ae1918be56609356b9a21a1db9b2cb0f96da5613
simalaoshi <1742495214@qq.com>
2022-06-21 21:17:46 +0800

    1，提交第三方插件

---
+++ FrameClient/Assets/3rd/GameFrameworkDebugger/Component/DebuggerComponent.LogNode.cs
@@ -0,0 +1,95 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2020 Jiang Yin. All rights reserved.
+// Homepage: https://gameframework.cn/
+// Feedback: mailto:ellan@gameframework.cn
+//------------------------------------------------------------
+
+using GameFramework;
+using System;
+using UnityEngine;
+
+namespace UnityGameFramework.Runtime
+{
+    public sealed partial class DebuggerComponent
+    {
+        /// <summary>
+        /// 日志记录结点。
+        /// </summary>
+        public sealed class LogNode
+        {
+            private DateTime m_LogTime;
+            private int m_LogFrameCount;
+            private LogType m_LogType;
+            private string m_LogMessage;
+            private string m_StackTrack;
+
+            /// <summary>
+            /// 初始化日志记录结点的新实例。
+            /// </summary>
+            public LogNode(LogType logType, string logMessage, string stackTrack)
+            {
+                m_LogTime = DateTime.Now;
+                m_LogFrameCount = Time.frameCount;
+                m_LogType = logType;
+                m_LogMessage = logMessage;
+                m_StackTrack = stackTrack;
+            }
+
+            /// <summary>
+            /// 获取日志时间。
+            /// </summary>
+            public DateTime LogTime
+            {
+                get
+                {
+                    return m_LogTime;
+                }
+            }
+
+            /// <summary>
+            /// 获取日志帧计数。
+            /// </summary>
+            public int LogFrameCount
+            {
+                get
+                {
+                    return m_LogFrameCount;
+                }
+            }
+
+            /// <summary>
+            /// 获取日志类型。
+            /// </summary>
+            public LogType LogType
+            {
+                get
+                {
+                    return m_LogType;
+                }
+            }
+
+            /// <summary>
+            /// 获取日志内容。
+            /// </summary>
+            public string LogMessage
+            {
+                get
+                {
+                    return m_LogMessage;
+                }
+            }
+
+            /// <summary>
+            /// 获取日志堆栈信息。
+            /// </summary>
+            public string StackTrack
+            {
+                get
+                {
+                    return m_StackTrack;
+                }
+            }
+        }
+    }
+}
---
+++ FrameClient/Assets/3rd/GameFrameworkDebugger/Component/DebuggerComponent.LuaWindow.cs
@@ -0,0 +1,57 @@
+﻿using GameFramework.Debugger;
+using System;
+using UnityEngine;
+
+namespace UnityGameFramework.Runtime
+{
+    public sealed partial class DebuggerComponent
+    {
+        [Serializable]
+        private sealed class LuaWindow : IDebuggerWindow
+        {
+            private string _lua = string.Empty;
+            private string _tips = "这是一个快捷调用lua的窗口，只要把你的lua代码复制到下面的框框，点击执行，就能调用到lua堆栈，方便在真机上测试";
+            public void Initialize(params object[] args)
+            {
+            }
+
+            public void OnDraw()
+            {
+                
+
+                GUILayout.BeginVertical("box");
+                {
+                    GUILayout.TextArea(_tips);
+                    _lua = GUILayout.TextField(_lua);
+                }
+                GUILayout.EndVertical();
+                if (GUILayout.Button("执行"))
+                {
+                    if (!string.IsNullOrEmpty(_lua))
+                    {
+                        //todo 通过LuaState doString
+                        _lua = string.Empty;
+                    }
+                        
+                }
+            }
+
+            public void OnEnter()
+            {
+            }
+
+            public void OnLeave()
+            {
+            }
+
+            public void OnUpdate(float elapseSeconds, float realElapseSeconds)
+            {
+            }
+
+            public void Shutdown()
+            {
+                _lua = string.Empty;
+            }
+        }
+    }
+}
